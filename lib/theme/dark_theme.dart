182672844156f84f733249d9f367e2cb940af3c6
hsufyan <97500262+hsufyan@users.noreply.github.com>
2023-09-19 19:49:44 +0500

    initial commit

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sixam_mart/util/app_constants.dart';
+
+ThemeData dark({Color color = const Color(0xFFE3D7F1)}) => ThemeData(
+      fontFamily: AppConstants.fontFamily,
+      primaryColor: color,
+      secondaryHeaderColor: const Color(0xFF009f67),
+      disabledColor: const Color(0xffa2a7ad),
+      brightness: Brightness.dark,
+      hintColor: const Color(0xFFbebebe),
+      cardColor:   Colors.black,
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: color)),
+      colorScheme: ColorScheme.dark(primary: color, secondary: color)
+          .copyWith(background:  Colors.black)
+          .copyWith(error: const Color(0xFFdd3135)),
+    );
